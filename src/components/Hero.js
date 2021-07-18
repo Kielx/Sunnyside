1ec21b700b98bc720230696c6f2e478aa7535bdf
kielx <kiel66@gmail.com>
2021-07-18 18:29:55 +0200

    style hero for desktop

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
 
   return (
     <div
-      className="app bg-bottom w-full h-auto p-0 m-0 pb-28 font-display text-white bg-cover bg-no-repeat"
+      className="md:bg-center md:h-screen app bg-bottom w-full h-auto p-0 m-0 pb-28 font-display text-white bg-cover bg-no-repeat"
       style={{
         backgroundImage:
           checkScreenWidth > 768
@@ -19,7 +19,7 @@ const Hero = () => {
       }}
     >
       <Navbar></Navbar>
-      <h1 className="text-3xl mb-4 mx-auto text-center font-text uppercase text-white tracking-widest">
+      <h1 className="md:pt-6 md:pb-24 md:text-5xl text-3xl mb-4 mx-auto text-center font-text uppercase text-white tracking-widest">
         We are creatives{" "}
       </h1>
       <svg
